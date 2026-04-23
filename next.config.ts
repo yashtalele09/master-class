ca65cddabb5b661e511f6a4f82808345f2fbac03
Yash Talele <124755377+yashtalele09@users.noreply.github.com>
2026-04-23 16:12:01 +0530

    Decap CMS

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["img.freepik.com"],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/admin/config.yml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "text/yaml",
+          },
+        ],
+      },
+    ];
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/admin",
+        destination: "/admin/index.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
